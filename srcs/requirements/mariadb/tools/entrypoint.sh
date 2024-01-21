51fe482ed5f6107f3c68a9d8cd4f871439c89b9a
D4rkSantana <emersonss13@gmail.com>
2024-01-21 20:10:36 -0300

    fix errors

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,42 +1,37 @@
 #!/bin/bash
 
 #incializa o mariadb em segundo plano
-#mysqld_safe --slip-syslog &
-service mariadb start
+echo "= Inicio do script"
+mysqld_safe --skip-syslog &
+echo "= Inicialização do MariaDB"
 
-echo "passo1"
 # Espera até que o MariaDB esteja pronto
 while ! mysqladmin ping -hlocalhost --silent; do
 	sleep 1
 done
 
-echo "passo2"
+echo "= Inicializacao completa do MariaDB"
 
 #Caso a não exista a base de dados, ele cria a base e cria um usuario
 #case ja exista, ele avisa com um echo
 if ! mysql -e "USE $DATABASE_NAME;";
 
 then
-	echo "c1"
+	echo "= Criacao de um novo Database"
 	mysql -e "CREATE DATABASE $DATABASE_NAME;"
-	echo "c2"
 	mysql -e "CREATE USER '$DB_ADMIN_USER'@'%' IDENTIFIED BY '$DB_ADMIN_PWD';"
-	echo "c3"
 	mysql -e "GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$DB_ADMIN_USER'@'%';"
-	echo "c4"
 	mysql -e "FLUSH PRIVILEGES;"
-	echo "c5"
 
-	echo "Database created."
+	echo "= Criacao completa do Database"
 else
-	echo "Database '$DATABASE_NAME' has already been created"
+	echo "= Database '$DATABASE_NAME' ja existe"
 fi
 
-echo "passo 3"
+echo "= Desligamento do banco"
 #desliga o banco
 kill $(cat /var/run/mysqld/mysqld.pid)
 
-echo "passo 4"
+echo "= Religamento do banco"
 #religa o banco de modo que o conteiner permanessa ativo apos o fim do script
-#exec mariadbd
-mysqld
+exec mysqld
--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -2,34 +2,25 @@
 
 echo "Inicio"
 if [ ! -f "/var/www/html/wp-config.php" ]; then
-	
-	echo "entrou no if"
+
 	#limpa completamente o /var/www/html antes da instalacao
 	rm -rf *.*
-	
-	echo "passou 1"
-	#faz o download do core do wordpress
-	#wp core download --allow-root
-	
-	wget -q http://wordpress.org/latest.tar.gz
-	tar xfz latest.tar.gz
-	mv wordpress/* .
-	rm -rf latest.tar.gz wordpress
 
-	echo "passou 2"
+	echo "= Inicio do download do Wordpress"
+	#faz o download do core do wordpress
+	wp core download --allow-root
+	echo "= Download concluido"
+
 	#renomeia o wp-config.php
 	mv wp-config-sample.php wp-config.php
+	echo "= wp-config.php renomeado"
 
-	echo "passou 3"
 	#faz as modificacoes com base nas envs com os dados personalizados para a instalacao do wordpress
 	sed -i "s/database_name_here/$DATABASE_NAME/g" wp-config.php
-	echo "passou 4a"
         sed -i "s/username_here/$DB_ADMIN_USER/g" wp-config.php
-	echo "passou 4b"
         sed -i "s/password_here/$DB_ADMIN_PWD/g" wp-config.php
-	echo "passou 4c"
         sed -i "s/localhost/$WP_HOST/g" wp-config.php
-	echo "passou 4d"
+	echo "= alteracoes no wp-config.php"
 
 	#instala o wordpress (cria um user admin no processo)
 	wp core install --allow-root \
@@ -39,25 +30,26 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
             --admin_email=$DB_ADMIN_EMAIL \
             --admin_password=$DB_ADMIN_PWD \
             --skip-email
+	echo "= Instalacao do Wordpress"
 
-	echo "passou 5"
 	#cria um novo usuario
 	wp user create --allow-root $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASSWORD --display_name=$WP_USER
+	echo "= Criacao de um novo usuario"
 
-	echo "passou 6"
 	#Desinstala o plugins padrao akisme e hello do wordpress
 	wp plugin uninstall akismet hello --allow-root
+	echo "= Desinstalados plugins akismet e hello"
 
-	echo "passou 7"
 	#atualiza os plugins
         wp plugin update --all --allow-root
+	echo "= Atualizacao dos plugins"
 
-	echo "passou 8"
 	#modifica o propietario e o grupo da pasta para www-data
         chown -R www-data:www-data /var/www/html
+	echo "= Modificacao nos proprietatios e grupo do /var/www/html"
 
 fi
 
-echo "fim"
+echo "== Configuracao Finalizada"
 #inicializa o php-fpm
 exec php-fpm7.4 -F
